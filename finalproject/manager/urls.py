07b93d3840d3941d07f3cad3bfd3c158c005772c
Joao Ottoni <jvoo2016@gmail.com>
2025-02-16 19:01:28 -0300

    Added Login, Register, Logout

--- finalproject/manager/urls.py
+++ finalproject/manager/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from . import views
+from manager.views import auth_views, views
 
 urlpatterns = [
-    
+    path("", views.index, name="index"),
+
+    # authentication views
+    path("login", auth_views.login_view, name="login"),
+    path("logout", auth_views.logout_view, name="logout"),
+    path("register", auth_views.register, name="register")
 ]
 
